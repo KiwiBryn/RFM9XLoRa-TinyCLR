23b1ef716acbad8bdc0fb9522c18ea3b6e2e3015
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-07-19 21:54:30 +1200

    Added Fezportal support and fix of interrupt oddness

--- ReceiveTransmitInterrupt/Program.cs
+++ ReceiveTransmitInterrupt/Program.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Need one of TINYCLR_V2_SC20100DEV_MIKROBUS_1/TINYCLR_V2_SC20100DEV_MIKROBUS_2/TINYCLR_V2_FEZDUINO defined
+// Need one of TINYCLR_V2_SC20100DEV_MIKROBUS_1/TINYCLR_V2_SC20100DEV_MIKROBUS_2/TINYCLR_V2_FEZDUINO/TINYCLR_V2_FEZPORTAL defined
 //---------------------------------------------------------------------------------
 namespace devMobile.IoT.Rfm9x.ReceiveTransmitInterrupt
 {
@@ -195,6 +195,9 @@ namespace devMobile.IoT.Rfm9x.ReceiveTransmitInterrupt
 #endif
 #if TINYCLR_V2_FEZDUINO
          Rfm9XDevice rfm9XDevice = new Rfm9XDevice(SC20100.SpiBus.Spi6, SC20100.GpioPin.PB1, SC20100.GpioPin.PA15, SC20100.GpioPin.PA1);
+#endif
+#if TINYCLR_V2_FEZPORTAL
+         Rfm9XDevice rfm9XDevice = new Rfm9XDevice(SC20100.SpiBus.Spi3, SC20100.GpioPin.PC13, SC20100.GpioPin.PD4,SC20100.GpioPin.PC2);
 #endif
          int sendCount = 0;
 
